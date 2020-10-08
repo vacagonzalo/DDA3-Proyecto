490efc1e78357ab2997a798c00131f9f41cca2ab
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2020-10-07 21:42:49 -0300

    esp32 connected with api

--- concrete-device/main.cpp
+++ concrete-device/main.cpp
@@ -1,24 +1,36 @@
-// docker run -it -p 1883:1883 -p 9001:9001 eclipse-mosquitto
-// mqtt pub -t 'test' -h 'localhost' -m 'test'
+// docker run rm -it -p 1883:1883 -p 9001:9001 eclipse-mosquitto
+// mqtt pub -t 'orders' -h 'localhost' -m '{"n":"esp32","a":true}'
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h> // PubSubClient by Nick O'Leary 
+#include <PubSubClient.h> // PubSubClient by Nick O'Leary
                           // https://github.com/knolleary/pubsubclient
+
+#include <ArduinoJson.h>  // ArduinoJson by  Benoit Blanchon
+                          // https://github.com/bblanchon/ArduinoJson
 
 #define LED 2
 #define TIME 1000
 #define BAUD_RATE 9600
 #define RECONNECT_TIME 5000
 
-const char *SSID = "Flia-Vaca";
-const char *PASSWORD = "ragnarok";
+#define TEMP_MAX 27
+#define TEMP_MIN 22
+
+#define HUM_MAX 70
+#define HUM_MIN 50
+
+const char *SSID = "Flia-Vaca";    // <- your WiFi
+const char *PASSWORD = "ragnarok"; // <- your WiFi's password
 const char *PRONT_CONNECTING = "Connecting to WiFi...";
 const char *PRONT_CONNECTED = "Connected to the WiFi network";
 
 const char *MQTT_SERVER = "192.168.0.9";
 const int MQTT_PORT = 1883;
 const char *CLIENT_ID = "esp32";
-const char *TOPIC = "test";
+const char *TOPIC = "readings";
+const char *S_TOPIC = "orders";
+
+bool actuator = true;
 
 WiFiClient MQTTclient;
 PubSubClient client(MQTTclient);
@@ -30,7 +42,7 @@ void callback(char *topic, byte *payload, unsigned int length)
   {
     payload_buff = payload_buff + String((char)payload[i]);
   }
-  Serial.println(payload_buff);
+  Serial.println(payload_buff);  
 }
 
 long lastReconnectAttempt = 0;
@@ -39,7 +51,7 @@ boolean reconnect()
 {
   if (client.connect(CLIENT_ID))
   {
-    client.subscribe(TOPIC);
+    client.subscribe(S_TOPIC);
   }
   return client.connected();
 }
@@ -75,8 +87,22 @@ void loop()
   }
   else
   {
+
     client.loop();
-    client.publish(TOPIC, "echo...");
+    String t = String(random(TEMP_MIN, TEMP_MAX));
+    String h = String(random(HUM_MIN, HUM_MAX));
+    String a;
+    if (actuator)
+    {
+      a = "true";
+    }
+    else
+    {
+      a = "false";
+    }
+    String temporal = "{\"n\":\"esp32\",\"t\":";
+    temporal = String(temporal + String(t) + ",\"h\":" + String(h) + ",\"a\":" + a) + "}";
+    client.publish(TOPIC, temporal.c_str());
     delay(TIME);
   }
 }
